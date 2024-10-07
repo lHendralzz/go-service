70cd3f48d230dd691f7b31f84e565db6864348fc
Hendra Effendi <hendraeffendi96@gmail.com>
2024-10-07 23:13:02 +0700

    fix : query create table missing TABLE keyword

--- ddl/create_users_table.sql
+++ ddl/create_users_table.sql
@@ -16,32 +16,32 @@ CREATE TABLE `product` (
 
 CREATE TABLE `carts` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL ,
   PRIMARY KEY (`id`)
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `cart_details` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `cart_id` int NOT NULL AUTO_INCREMENT,
-  `product_id` int NOT NULL AUTO_INCREMENT,
-  `quantity` int NOT NULL AUTO_INCREMENT,
+  `cart_id` int NOT NULL ,
+  `product_id` int NOT NULL ,
+  `quantity` int DEFAULT 0,
   PRIMARY KEY (`id`)
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE shop(
+CREATE TABLE shop(
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE warehouse(
+CREATE TABLE warehouse(
   `id` int NOT NULL AUTO_INCREMENT,
   `shop_id` int DEFAULT NULL,
   `name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE warehouse_detail(
+CREATE TABLE warehouse_detail(
   `id` int NOT NULL AUTO_INCREMENT,
   `warehouse_id` int DEFAULT NULL,
   `product_id` int DEFAULT NULL,
