20e896a366b0c2915833ffb08b49eb310664ee8b
Hendra Effendi <hendraeffendi96@gmail.com>
2024-10-08 19:51:46 +0700

    feat : add column store_id in order table

--- ddl/20241007_create_users_table.sql
+++ ddl/20241007_create_users_table.sql
@@ -16,7 +16,8 @@ CREATE TABLE `product` (
 
 CREATE TABLE `order` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL ,
+  `user_id` int NOT NULL,
+  `store_id` int NOT NULL,
   `status` int DEFAULT 1,
   PRIMARY KEY (`id`)
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
